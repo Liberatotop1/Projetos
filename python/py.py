758c06f03c962167a45a74b593e2170d5f65ef6a
Ysaac Liberato <110784834+Liberatotop1@users.noreply.github.com>
2024-02-01 15:46:36 -0300

    Add files via upload

---
+++ python/py.py
@@ -0,0 +1,16 @@
+from flask_corslask import Flask, jsonify, request
+
+app = Flask (__Nome__)
+
+livros = [
+    {
+        "id": 1,
+        "titulo": "o Senhor dos aneis - A sociedade do anel"
+        "autor": 'J. R. R Tolkien'
+
+    },
+    {
+        "id" : 2,
+        "ti"
+    }
+]
